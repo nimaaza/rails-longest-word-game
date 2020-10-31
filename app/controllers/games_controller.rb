37e011601f19329602a09ca4e3eb072f2333c7d4
Nima Azarbayjani <nimaza@gmail.com>
2020-10-31 21:34:41 +0330

    created the new route

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,12 @@
+class GamesController < ApplicationController
+  def new
+    grid_size = rand(5..15)
+    letters = ('A'..'Z').to_a
+    @letters = []
+    grid_size.times { @letters << letters[rand(0...26)] }
+  end
+
+  def score
+
+  end
+end
