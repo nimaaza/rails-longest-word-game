f6f866e3278a4ea887c207896ac9129d8f1a70f9
Nima Azarbayjani <nimaza@gmail.com>
2020-11-01 13:03:21 +0330

    added the scoring logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     grid_size = rand(5..15)
@@ -8,5 +11,25 @@ class GamesController < ApplicationController
 
   def score
     @letters = params[:letters].split(',')
+    word = params[:word].upcase
+    @message = if !build_from_grid?(word)
+                 "Sorry, but #{word} can't be built our of #{params[:letters]}..."
+               elsif !english_word?(word)
+                 "Sorry, but #{word} does not seem to be a valid English word..."
+               else
+                 "Congratulations, #{word} is a valid English word!"
+               end
+  end
+
+  private
+
+  def build_from_grid?(word)
+    word.split('').all? { |letter| @letters.include?(letter) }
+  end
+
+  def english_word?(word)
+    api_url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    api_response = JSON.parse(open(api_url).read)
+    api_response['found']
   end
 end
